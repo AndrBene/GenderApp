9d0a772d05cbf699c23e187a60e0a3546ec1ca83
Andrea Benedetto <andri.ben09@gmail.com>
2024-11-12 17:46:15 +0100

    Updated port

--- main.py
+++ main.py
@@ -13,4 +13,4 @@ app.add_url_rule(rule='/app/',endpoint='app',view_func=views.app)
 app.add_url_rule(rule='/app/gender/',endpoint='gender',view_func=views.genderapp,methods=['GET','POST'])
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=1239)
